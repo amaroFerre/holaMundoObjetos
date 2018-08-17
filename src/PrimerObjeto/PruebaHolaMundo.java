f667afdcd541a00f69920e1bf813abac13882fdb
SYSTEM <SYSTEM@equipo-zalman>
2018-08-17 07:58:24 +0200

    Imprimo clase por consola Hola mundo

---
+++ src/PrimerObjeto/PruebaHolaMundo.java
@@ -0,0 +1,25 @@
+package PrimerObjeto;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SYSTEM
+ */
+public class PruebaHolaMundo {
+    
+    public static void main(String[] args) {
+        
+        // Creo un objeto de la clase HolaMundo llamado mundos
+        HolaMundo mundos= new HolaMundo();
+        
+        //Imprimo la clase mundo
+        System.out.println(mundos.mundo);
+        
+    }
+    
+}
