05a64fff22b43614f6e1842fdac0c8bfd1fedc5e
SYSTEM <SYSTEM@equipo-zalman>
2018-08-17 17:45:10 +0200

    Adivina numero

---
+++ src/PrimerObjeto/HolaMundo.java
@@ -0,0 +1,24 @@
+package PrimerObjeto;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SYSTEM
+ */
+public class HolaMundo {
+    
+    String mundo;
+    
+    public HolaMundo() {
+        
+       mundo = "Hola mundo";
+    
+    
+}
+    
+}
